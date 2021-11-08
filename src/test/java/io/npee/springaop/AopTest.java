fbc6a4a3c5c1482fd70ed39965dda076fb1d2306
npee <jmc4664@gmail.com>
2021-11-08 22:33:09 +0900

    스프링 AOP - 기본 Aspect 적용

--- src/test/java/io/npee/springaop/AopTest.java
+++ src/test/java/io/npee/springaop/AopTest.java
@@ -2,16 +2,19 @@ package io.npee.springaop;
 
 import io.npee.springaop.order.OrderRepository;
 import io.npee.springaop.order.OrderService;
+import io.npee.springaop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
@@ -21,7 +24,7 @@ public class AopTest {
     OrderRepository orderRepository;
 
     @Test
-    void aopInfo() { 
+    void aopInfo() {
         log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
         log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
     }
