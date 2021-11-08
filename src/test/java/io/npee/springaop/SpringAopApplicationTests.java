c074aa3c5ddb0204c798bfa85a234e7e1356e34a
npee <jmc4664@gmail.com>
2021-11-08 22:03:31 +0900

    Initial Commit

---
+++ src/test/java/io/npee/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
